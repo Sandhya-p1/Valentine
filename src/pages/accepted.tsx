d10845799a913ab863daed7a9805ba0322b55cd3
sandhya <p91sandhya@gmail.com>
2026-02-03 15:02:19 +0545

    created ui for accepted  page

--- src/pages/accepted.tsx
+++ src/pages/accepted.tsx
@@ -1,5 +1,17 @@
 const Accepted = () => {
-  return <div className="text-red-500 font-bold text-6xl">Accepted</div>;
+  return (
+    <div className="min-h-screen p-12 bg-linear-to-r from-pink-300 via-pink-100 to-pink-200">
+      <div className="absolute bottom-0 ">
+        <img src="/images/Adobe Express - file.png" />
+      </div>
+      <div className="text-pink-800 font-bold absolute  right-[10%] top-1/3">
+        <h2 className="text-6xl ">I Knew It🫣</h2>
+        <p className="text-center mt-4 font-semibold text-4xl animate-pulse ">
+          You’re Officially My Valentine 💗
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Accepted;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,21 +11,31 @@ const Home = () => {
   };
 
   return (
-    <div className="text-2xl bg-white rounded-3xl shadow-xl shadow-pink-200 p-12  ">
-      <div className="flex flex-col gap-y-10 items-center justify-center">
-        <h1 className="text-5xl text-pink-500 font-bold">
-          Will You Be My Valentine ?
-        </h1>
-        <div className="flex items-center space-x-8 text-red-400">
-          <button
-            onClick={handleYesClick}
-            className="px-8 py-2 text-2xl hover:scale-150 transition-transform ease-linear duration-300 rounded-2xl  bg-pink-100 font-semibold"
-          >
-            Yes 💗
-          </button>
-          <button className="px-8 py-2  text-2xl rounded-2xl bg-pink-100 font-semibold">
-            No 😭
-          </button>
+    <div className="home-bg min-h-screen flex items-center justify-center w-screen">
+      <div className=" text-2xl border  rounded-3xl shadow-xl shadow-pink-200 px-12 py-20  ">
+        <div className="flex flex-col gap-y-10 items-center justify-center">
+          <h1 className="text-6xl text-pink-500 font-bold">
+            Will You Be My Valentine ?💗
+          </h1>
+          <div className="flex items-center space-x-8 text-red-400">
+            <button
+              onClick={handleYesClick}
+              className="cursor-pointer px-8 py-2 text-3xl hover:scale-150 transition-transform ease-linear duration-300 rounded-2xl  bg-pink-100 font-semibold"
+            >
+              Yes 💗
+            </button>
+            <button
+              onMouseEnter={(e) => {
+                const x = Math.random() * 300 - 100;
+                const y = Math.random() * 300 - 100;
+
+                e.currentTarget.style.transform = `translate(${x}px, ${y}px)`;
+              }}
+              className=" cursor-not-allowed px-8 py-2 text-3xl rounded-2xl  bg-pink-100 font-semibold transition-transform duration-300 ease-out "
+            >
+              No 😭
+            </button>
+          </div>
         </div>
       </div>
     </div>
